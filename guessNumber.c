f3e491f50c5ab7c3b63664a13afb75fc1a87a3d7
Natasha Kalosi <kalosinatasha@gmail.com>
2026-04-02 00:17:26 +0200

    Add files via upload

---
+++ guessNumber.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int secretNum, guess, attempts = 0;
+    
+    // generate random number
+    srand(time(0)); 
+    secretNum = rand() % 100 + 1;
+
+    printf("Guess the number between 1-100\nDon't worry I'll help you.");
+
+    do {
+        printf("Your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        int difference=abs(secretNum-guess);
+        
+        if (difference>11) {
+            printf("Nope. Too far.\n");
+        } else if (difference<10) {
+            printf("You're really close.\n");
+        } else {
+            printf("Congratlations! Found it on your %d try.\n", attempts);
+        }
+    } while (guess != secretNum);
+
+    return 0;
+}
